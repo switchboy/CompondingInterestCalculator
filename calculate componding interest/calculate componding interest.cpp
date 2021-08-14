6016772aceeb51900f5b8bee7ba933311f979024
Daan Riegman <riegman_d@hotmail.com>
2021-08-14 22:15:05 +0200

    Basic structuring

--- calculate componding interest/calculate componding interest.cpp
+++ calculate componding interest/calculate componding interest.cpp
@@ -41,39 +41,54 @@ public:
 };
 
 
-bool is_number(const std::string& s)
+bool isValidDouble(const std::string& s)
 {
-    return !s.empty() && std::find_if(s.begin(),
-        s.end(), [](unsigned char c) { return !std::isdigit(c); }) == s.end();
+    char* end = nullptr;
+    double val = strtod(s.c_str(), &end);
+    return end != s.c_str() && *end == '\0' && val != HUGE_VAL;
+}
+
+void askAndGetAnswerFromConsole(std::string question, double& answer) {
+    std::string myNumberString;
+    bool numberEnterd = false;
+    while (!numberEnterd) {
+        std::cout << question << std::endl;
+        std::getline(std::cin, myNumberString);
+        if (isValidDouble(myNumberString)) {
+            answer = std::stod(myNumberString);
+            std::cout << std::endl;
+            numberEnterd = true;
+        }
+        else {
+            std::cout << "Please enter only number and a point: use the folowing format 999.99!" << std::endl << std::endl;
+        }
+    }
+}
+
+void getStartingValuesFromUser(){
+    double  amountInSavingsAccount;
+    double  yearlyInterestSavingsAccount;
+    double  amountInvested;
+    double  yearlyAverageYieldInvestment;
+    double  valueOfHouse;
+    askAndGetAnswerFromConsole("How much money is in your savings account right now:", amountInSavingsAccount);
+    askAndGetAnswerFromConsole("What has been the avarage interest on this savings account: (%)", yearlyInterestSavingsAccount);
+    askAndGetAnswerFromConsole("How much money is in your investment portofolio right now:", amountInvested);
+    askAndGetAnswerFromConsole("What is the expected yield of your portofolio: (%)", yearlyAverageYieldInvestment);
+    askAndGetAnswerFromConsole("What is the value of your home:", valueOfHouse);
+}
+
+void displayWelcomeText() {
+    std::cout << "Finance calculator v 0.1" << std::endl;
+    std::cout << "Calculate the effect of compounding interest and inflation on buying power in \"Today's money\" over a given number of \n" <<
+        "years. This wil help you eveluate your savings and investment plan. It will note the years from now when you could \n" <<
+        "expect to retire with your current lifestyle. This will be three percent annualy of your total capital" << std::endl << std::endl << std::endl;
 }
 
 int main()
 {
-    std::string myNumberString;
-    std::cout << "Finance calculator v 0.1" << std::endl;
-    std::cout << "Calculate the effect of compounding interest and inflation on buying power in \"Today's money\" over a given number of \n" <<
-                 "years. This wil help you eveluate your savings and investment plan. It will note the years from now when you could expect \n"<<
-                 "to retire with your current lifestyle. This will be three percent annualy of your total capital" << std::endl;
-    
+    displayWelcomeText();
+    getStartingValuesFromUser();
+    system("pause");
 
-    std::cout << "How much money is in your savings account right now:" <<std::endl;
-    std::getline(std::cin, myNumberString);
-    int  amountInSavingsAccount  = std::stoi(myNumberString);
-
-    std::cout << "What has been the avarage interest on this savings account: (%)" << std::endl;
-    std::getline(std::cin, myNumberString);
-    int  yearlyInterestSavingsAccount = std::stoi(myNumberString);
-
-    std::cout << "How much money is in your investment portofolio right now:" << std::endl;
-    std::getline(std::cin, myNumberString);
-    int  amountInvested = std::stoi(myNumberString);
-
-    std::cout << "What is the expected yield of your portofolio: (%)" << std::endl;
-    std::getline(std::cin, myNumberString);
-    int  yearlyAverageYieldInvestment = std::stoi(myNumberString);
-
-    std::cout << "What is the value of your home:" << std::endl;
-    std::getline(std::cin, myNumberString);
-    int  valueOfHouse = std::stoi(myNumberString);
-    
 }
