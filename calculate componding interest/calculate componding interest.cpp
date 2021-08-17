59205f97f8044db9df6f6e58c80c3e6397e29ac4
Daan Riegman <riegman_d@hotmail.com>
2021-08-17 20:44:04 +0200

    word wrapping

--- calculate componding interest/calculate componding interest.cpp
+++ calculate componding interest/calculate componding interest.cpp
@@ -295,8 +295,8 @@ void displayWelcomeText() {
     std::cout << "Finance calculator v 0.2" << std::endl;
     std::cout << "Calculate the effect of compounding interest and inflation on buying power in \"Today's money\" over a given number of \n" <<
         "years. This wil help you eveluate your savings and investment plan. It will note the years from now when you could \n" <<
-        "expect to retire with your current lifestyle. This will be when 3% your net worth is equal or more then your current \n  " << 
-        "net income after mortgage payment. Assuming this will be payed off by then, otherwise the remaining mortgage is also \n "<<
+        "expect to retire with your current lifestyle. This will be when 3% your net worth is equal or more then your current \n" << 
+        "net income after mortgage payment. Assuming this will be payed off by then, otherwise the remaining mortgage is also \n"<<
         "added to the net income the 3% needs to match. Note that this only applies when your yield is more then 3%"<<
         std::endl << std::endl << std::endl;
 }
